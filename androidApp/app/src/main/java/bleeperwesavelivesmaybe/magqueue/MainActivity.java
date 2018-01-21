1f42678f4306937e5105a6f3d4055f09205b2689
Drapha <danielisakov@yahoo.ca>
2018-01-20 19:28:15 -0500

    Added basic android app framework

---
+++ androidApp/app/src/main/java/bleeperwesavelivesmaybe/magqueue/MainActivity.java
@@ -0,0 +1,49 @@
+package bleeperwesavelivesmaybe.magqueue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+import android.app.Activity;
+import android.view.View;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView response;
+    EditText editTextAddress, editTextPort;
+    Button buttonConnect, buttonClear;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextAddress = (EditText) findViewById(R.id.addressEditText);
+        editTextPort = (EditText) findViewById(R.id.portEditText);
+        buttonConnect = (Button) findViewById(R.id.connectButton);
+        buttonClear = (Button) findViewById(R.id.clearButton);
+        response = (TextView) findViewById(R.id.responseTextView);
+
+        buttonConnect.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Client myClient = new Client(editTextAddress.getText()
+                        .toString(), Integer.parseInt(editTextPort
+                        .getText().toString()), response);
+                myClient.execute();
+            }
+        });
+
+        buttonClear.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                response.setText("");
+            }
+        });
+    }
+}
